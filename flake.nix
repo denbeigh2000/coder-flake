1f3f71dfa6e0d60f834f62641e119732669fb663
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-09-16 20:53:06 -0700

    add more varieties

--- flake.nix
+++ flake.nix
@@ -16,14 +16,25 @@
   outputs = { self, nixpkgs, flake-utils, coder }:
     flake-utils.lib.eachDefaultSystem (system:
     let
+      version = "0.8.15";
       pkgs = import nixpkgs { inherit system; };
       mkCoder = import ./build.nix;
     in
       with pkgs; {
         packages = {
-          coder = mkCoder {
-            inherit pkgs coder;
-            version = "0.8.15";
+          coder = mkCoder { inherit pkgs coder version; };
+          coder-slim = mkCoder {
+            inherit pkgs coder version;
+            slim = true;
+          };
+          coder-agpl = mkCoder {
+            inherit pkgs coder version;
+            agpl = true;
+          };
+          coder-slim-agpl = mkCoder {
+            inherit pkgs coder version;
+            slim = true;
+            agpl = true;
           };
         };
       });
